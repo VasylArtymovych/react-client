7af6a06e68d5ae2270fe60203d9545865d66d0cc
VasylArtymovych <art777vasss@gmail.com>
2022-10-08 20:13:53 +0300

    add start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <LoginPage />
+      <h2>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
